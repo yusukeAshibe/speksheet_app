87c5c103aeb708a43dedadf9475a0e1f871c7579
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jp  git config --list git config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-10-09 16:33:31 +0900

    指摘事項の修正

--- src/Batch/SpecSheet.java
+++ src/Batch/SpecSheet.java
@@ -1,4 +1,4 @@
-package Batch;
+package batch;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -7,12 +7,26 @@ public class SpecSheet {
 	public static void main(String[] args) {
 
 		// 第1階層
-		String firstPath = "C:\\specsheet\\";
+		// String firstPath = "C:\\specsheet";
 
+		String firstPath = null;		
+		if (args.length == 0) {//引数なしの場合はカレントディレクトリを代入
+			firstPath = new File(".").getAbsoluteFile().getParent();
+			searchPersonalEngineerIdAndSpecSheetUpdateDate(firstPath);
+		} else if (args.length == 1) {
+			firstPath = args[0];
+			searchPersonalEngineerIdAndSpecSheetUpdateDate(firstPath);
+		} else {
+			System.out.println("引数はなし（カレントディレクトリになる）もしくは1つ" + "（01.NW or 02.java or 04.ML までのPath ）を指定してください");
+		}
+
+	}
+
+	static void searchPersonalEngineerIdAndSpecSheetUpdateDate(String firstPath) {
 		// 第2階層
-		String PathNW = "01.NW\\";
-		String PathJava = "02.java\\";
-		String PathML = "04.ML\\";
+		String PathNW = "\\01.NW\\";
+		String PathJava = "\\02.java\\";
+		String PathML = "\\04.ML\\";
 
 		String[] secondPathList = { PathNW, PathJava, PathML };
 
@@ -27,12 +41,10 @@ public class SpecSheet {
 					targetFolder = new File(firstPath + secondPath + thirdPath);
 					// 最終階層
 					for (String fourthPath : targetFolder.list()) {
-						if (fourthPath.matches("NW-\\d{3}-\\d{4}.xls.*$")
-								|| fourthPath.matches("AP-\\d{3}-\\d{4}.xls.*$")
-								|| fourthPath.matches("ML-\\d{3}-\\d{4}.xls.*$")) { // （例）NW_201_9999.xls(x)
+						if (fourthPath.matches("[A-Z]{2}-\\d{3}-\\d{4}.xls.*$")) { // （例）NW_201_9999.xls(x) ||
 
 							String[] lastPathList = fourthPath.split("-", 3);// 最終階層を - で分割
-							String lastPath = lastPathList[lastPathList.length - 1];// 分割した最後を取得
+							String lastPath = lastPathList[2];// 分割した前から三番目の文字列を取得
 							String personalEngineerId = lastPath.substring(0, 4);// 頭の4桁の数字を取得
 
 							// 最終的なpath
